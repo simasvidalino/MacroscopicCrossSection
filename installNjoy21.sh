3b09c108347c0931f3e845df7fa5c0c1e88c3e39
simasvidalino <andreiasimas8@gmail.com>
2021-12-29 14:41:24 -0300

    installNjoy21: change apt-get to apt install '#stop

--- installNjoy21.sh
+++ installNjoy21.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-sudo apt-get install gfortran
-sudo apt-get install python3
-sudo apt-get install gcc
-sudo apt-get install cmake
-git clone https://github.com/njoy/NJOY21.git
-wget https://raw.githubusercontent.com/njoy/signatures/master/NJOY21/1.1.0-NJOY21.json
+sudo apt install gfortran
+sudo apt install python3
+sudo apt install gcc
+sudo apt install cmake
+git clone --branch v1.2.1 https://github.com/njoy/NJOY21.git
 cd NJOY21
-./metaconfigure/fetch_subprojects.py ../1.1.0-NJOY21.json
 mkdir bin
 cd bin
-cmake -D fetched_subprojects=true ../
+cmake -D CMAKE_BUILD_TYPE=Release ..
 make
 make test
