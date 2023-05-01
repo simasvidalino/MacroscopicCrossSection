de800ef15325953d684dc097039e343a36e32512
Andréia de Simas Vidalino <78229980+simasvidalino@users.noreply.github.com>
2023-05-01 12:16:01 -0300

    MacroscopicCrossSection: Add install script
    Add install script for FEDORA SO

---
+++ installNjoy21FEDORA.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo dnf upgrade
+sudo dnf install gfortran
+sudo dnf install python3
+sudo dnf install gcc
+sudo dnf install cmake
+git clone --branch v1.2.2 https://github.com/njoy/NJOY21.git
+cd NJOY21
+mkdir bin
+cd bin
+cmake -D CMAKE_BUILD_TYPE=Release ..
+make
+make test
